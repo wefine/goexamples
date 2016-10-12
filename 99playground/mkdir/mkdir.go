be668da88f51a8ceaa8304c96159eab91375d016
wefine <wang.xiaoren@zte.com.cn>
2016-10-12 14:18:12 +0800

    add mkdir for linux

---
+++ 99playground/mkdir/mkdir.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func main() {
+	// for linux
+	mask := syscall.Umask(0)
+	defer syscall.Umask(mask)
+	err := os.MkdirAll("/tmp/gotest/", 0777)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Mkdir /tmp/gotest/")
+}

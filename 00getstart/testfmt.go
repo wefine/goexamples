b22d417a38266796e124043592c7cac0917bd2b4
wefine <wang.xiaoren@zte.com.cn>
2016-11-11 16:53:02 +0800

    add fmt test
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

---
+++ 00getstart/testfmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("haha")
+
+	fmt.Errorf("expected Exit to be true, CheckpointI")
+
+	//var CheckpointID = 12341241
+	//fmt.Errorf("expected Exit to be true, CheckpointID= %v", CheckpointID)
+}

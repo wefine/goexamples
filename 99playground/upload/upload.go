a27d8c2cf378a117598efd8c039cceec853112f9
wefine <wefine@aliyun.com>
2017-12-29 17:29:26 +0800

    upload binary demo

---
+++ 99playground/upload/upload.go
@@ -0,0 +1,56 @@
+// curl -X POST -H "Content-Type: application/octet-stream" --data-binary '@filename' http://127.0.0.1:5050/upload
+
+package main
+
+import (
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+    "io/ioutil"
+    "time"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+    defer r.Body.Close()
+
+    file, err := os.Create("d:/u.tar")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    n, err := io.Copy(file, r.Body)
+    if err != nil {
+        panic(err)
+    }
+
+    w.Write([]byte(fmt.Sprintf("%d bytes are recieved.\n", n)))
+}
+
+func main() {
+    http.HandleFunc("/upload", uploadHandler)
+
+    go func() {
+        time.Sleep(time.Second * 1)
+        upload()
+    }()
+
+    http.ListenAndServe(":5050", nil)
+}
+
+func upload() {
+    file, err := os.Open("c:/a.tar")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    res, err := http.Post("http://127.0.0.1:5050/upload", "binary/octet-stream", file)
+    if err != nil {
+        panic(err)
+    }
+    defer res.Body.Close()
+    message, _ := ioutil.ReadAll(res.Body)
+    fmt.Printf(string(message))
+}

67adb5f732efd6ed0022d7d5e3c8089075866925
wefine <wefine@aliyun.com>
2018-02-06 18:23:27 +0800

    goweb, update HandleFunc test

--- goweb/ch01/web/main.go
+++ goweb/ch01/web/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "time"
+    "log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    log.Println("hello")
+
 	fmt.Fprint(w, "Hello, world!")
+	time.Sleep(time.Second * 10)
 }
 
 func main() {
+    log.Println("loading...")
+    time.Sleep(time.Second * 10)
+    log.Println("loading...finished!")
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
